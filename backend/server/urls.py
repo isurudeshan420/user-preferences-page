912d665e7137840fe139ae578c32d73249bc647f
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2025-12-18 20:31:12 +0530

    url configured

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+# from rest_framework.documentation import include_docs_urls
+# from django.conf.urls.static import static
+from myapp.views import UserProfileViewSet, UserPreferencesViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+# Create a router and register our viewset with it.
+router = routers.DefaultRouter()
+
+router.register(r'users', UserProfileViewSet, basename='user')
+router.register(r'user-preferences', UserPreferencesViewSet, basename='user-preferences')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
+
+urlpatterns += [
+    # path('api/docs/', include_docs_urls(title='My API Documentation')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
